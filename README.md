ef4187f7286cc4e565e600faa2a9569395e5ec56
Arthur Haas <arthur.haas4@gmail.com>
2021-08-22 21:41:42 +0200

    add project from vuemasters real-world vue 3

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # learning_vuejs
 Some of my projects while learning vuejs
+
+## cheat sheet
+
+Yeah, I know.. basic commands don't need a cheat sheet. But I'm no JS developer, just a simple data scientist doing some frontend design for my projects. ;)
+
+```bash
+npm update -g @vue/cli  
+
+vue create *project name*
+
+npm run serve
+```
---
+++ real-world-vue/src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "../views/EventList.vue";
+import EventDetails from "../views/EventDetails.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: '/:catchAll(.*)', 
+    name: 'PageNotFound',
+    component: () =>
+      import(/* webpackChunkName: "pagenotfound" */ "../views/PageNotFound.vue"),
+    meta: {
+      requiresAuth: false
+    }
+  },
+
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
